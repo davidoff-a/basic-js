10b8c74fd2679b8da5b8276def8a86890e8906db
aliaksei davydau <davidoff.aleksei@gmail.com>
2021-02-24 15:41:08 +0300

    feat: what season task is DONE

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,30 @@
-const CustomError = require("../extensions/custom-error");
+'use strict';
+module.exports = function getSeason(date) {
+        let error = new Error('wrong type');
+        // let date = 'foo';
+        // let check = date instanceof Date;
+        const seasons = {
+            0: 'winter',
+            1: 'winter',
+            2: 'spring',
+            3: 'spring',
+            4: 'spring',
+            5: 'summer',
+            6: 'summer',
+            7: 'summer',
+            8: 'autumn',
+            9: 'autumn',
+            10: 'autumn',
+            11: 'winter'
+        };
+        if (date === undefined || date === '') {
+            return 'Unable to determine the time of year!';
+        } else {
+            if (date instanceof Date && !isNaN(date.valueOf())) {
+                return seasons[date.getMonth()];
+            } else {
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+            throw error;
+            }
+            }
 };
