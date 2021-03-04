479f7e12d91c040383c78d2ddaef9883ed25d1f8
Davydau Aliaksey <davidoff.aleksei@gmail.com>
2021-03-04 22:26:38 +0300

    feat: solve task carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,9 +1,29 @@
-const CustomError = require("../extensions/custom-error");
+'use strict';
+// const CustomError = require("../extensions/custom-error");
 
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+	// const sampleActivity = '1';
+
+	if (typeof data === String && data !== '') {
+		if (Number(data) && Number(data) > 0) {
+			return (
+				Math.log(MODERN_ACTIVITY / +sampleActivity) / (0.693 / HALF_LIFE_PERIOD)
+			);
+		}
+		return false;
+	} else {
+		return false;
+	}
 };
+	
+
+
+
+
+  
+
+
+// };
