1ac0f84e75bc006406f96b4a83ea3c53c32d18ac
Davydau Aliaksey <davidoff.aleksei@gmail.com>
2021-03-04 23:33:03 +0300

    feat:transform array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,42 +1,41 @@
 // 
 'use strict';
-const {
-  stackTraceLimit
-} = require("../extensions/custom-error");
-const CustomError = require("../extensions/custom-error");
-// const arr = [1, 2, 3, '--double-next', 1337, '--discard-prev', 4, 5];
-module.exports = function transform(arr) {
+// const {
+//   stackTraceLimit
+// } = require("../extensions/custom-error");
+// const CustomError = require("../extensions/custom-error");
+const arr = [1, 2, 3, '--double-next', 1337, '--discard-prev', 4, 5];
+// module.exports = function transform(arr) {
 if (!Array.isArray) {
   throw new Error('Error');
 }
-const massive = arr;
-// объявляем объект с инструкциями поле - инструкция, значение - метод обработки
-const methods = {
-  '--discard-next': discardNext,
-  '--discard-prev': discardPrev,
-  '--double-next': doubleNext,
-  '--double-prev': doublePrev
-};
-// объявляем общий индекс
+let massive = arr;
+let arrLen = massive.length;
 let index = 0;
-// перебираем элементы пока индекс меньше длины массива сравниваем с инструкциями в объекте 
-while (index < massive.length) {
-  // динамическая функция. если элемент равен названию метода, то назначается функция из объекта, иначе назначается функция инкремента индекса
-  const method = methods[massive[index]] || incrementIndex;
-  // вызываем метод
-  method();
+
+
+function incrementIndex() {
+	index++;
 }
-console.log(massive);
-//Возвращаем массив
-return massive;
 
+function isTheLast() {
+	return index === massive.length - 1;
+}
 
+function isFirst() {
+	return index === 0;
+}
+
+function removeCurrent() {
+	massive.splice(index, 1);
+}
 
 function discardNext() {
   if (isTheLast()) {
     removeCurrent();
   } else {
     massive.splice(index, 2);
+    arrLen = massive.length;
   }
 }
 
@@ -45,6 +44,8 @@ function discardPrev() {
     removeCurrent();
   } else {
     massive.splice(index - 1, 2);
+    arrLen = massive.length;
+		index--;
   }
 }
 
@@ -53,6 +54,7 @@ function doubleNext() {
     removeCurrent();
   } else {
     massive[index] = massive[index + 1];
+    arrLen = massive.length;
   }
 }
 
@@ -61,22 +63,29 @@ function doublePrev() {
     removeCurrent();
   } else {
     massive[index] = massive[index - 1];
+    arrLen = massive.length;
   }
 }
 
-function incrementIndex() {
-  index++;
-}
+const methods = {
+	'--discard-next': discardNext,
+	'--discard-prev': discardPrev,
+	'--double-next': doubleNext,
+	'--double-prev': doublePrev,
+};
+// перебираем элементы пока индекс меньше длины массива сравниваем с инструкциями в объекте
+while (index < arrLen) {
+	// динамическая функция. если элемент равен названию метода, то назначается функция из объекта, иначе назначается функция инкремента индекса
 
-function isTheLast() {
-  return index === massive.length - 1;
+	const method = methods[massive[index]] || incrementIndex;
+	// вызываем метод
+	method();
 }
+// console.log(massive);
+//Возвращаем массив
+return massive = massive.filter();
 
-function isFirst() {
-  return index === 0;
-}
 
-function removeCurrent() {
-  massive.splice(index, 1);
-}
-}
+
+
+// }
