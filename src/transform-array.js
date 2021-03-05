8d18b008987d2b683cb739aa6338d2db52712830
Davydau Aliaksey <davidoff.aleksei@gmail.com>
2021-03-05 21:38:41 +0300

    feat: add map method

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,4 @@
-// 
+//
 'use strict';
 // const {
 //   stackTraceLimit
@@ -7,85 +7,26 @@
 const arr = [1, 2, 3, '--double-next', 1337, '--discard-prev', 4, 5];
 // module.exports = function transform(arr) {
 if (!Array.isArray) {
-  throw new Error('Error');
+	throw new Error('Error');
 }
-let massive = arr;
-let arrLen = massive.length;
-let index = 0;
-
-
-function incrementIndex() {
-	index++;
+function isNum(elem) {
+	return typeof elem === 'number';
 }
+// const methods = {
+// 	'--discard-next': discardNext,
+// 	'--discard-prev': discardPrev,
+// 	'--double-next': doubleNext,
+// 	'--double-prev': doublePrev,
+// };
 
-function isTheLast() {
-	return index === massive.length - 1;
-}
-
-function isFirst() {
-	return index === 0;
-}
-
-function removeCurrent() {
-	massive.splice(index, 1);
-}
-
-function discardNext() {
-  if (isTheLast()) {
-    removeCurrent();
-  } else {
-    massive.splice(index, 2);
-    arrLen = massive.length;
+let massive;
+massive = arr.map(function (item, index) {
+	if (isNum(item)) {
+		console.log(item);
+	} else {
+    console.log(index);
   }
-}
-
-function discardPrev() {
-  if (isFirst()) {
-    removeCurrent();
-  } else {
-    massive.splice(index - 1, 2);
-    arrLen = massive.length;
-		index--;
-  }
-}
-
-function doubleNext() {
-  if (isTheLast()) {
-    removeCurrent();
-  } else {
-    massive[index] = massive[index + 1];
-    arrLen = massive.length;
-  }
-}
-
-function doublePrev() {
-  if (isFirst()) {
-    removeCurrent();
-  } else {
-    massive[index] = massive[index - 1];
-    arrLen = massive.length;
-  }
-}
-
-const methods = {
-	'--discard-next': discardNext,
-	'--discard-prev': discardPrev,
-	'--double-next': doubleNext,
-	'--double-prev': doublePrev,
-};
-// перебираем элементы пока индекс меньше длины массива сравниваем с инструкциями в объекте
-while (index < arrLen) {
-	// динамическая функция. если элемент равен названию метода, то назначается функция из объекта, иначе назначается функция инкремента индекса
-
-	const method = methods[massive[index]] || incrementIndex;
-	// вызываем метод
-	method();
-}
+});
 // console.log(massive);
-//Возвращаем массив
-return massive = massive.filter();
-
-
-
 
 // }
