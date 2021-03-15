f4330691e171177ea54661afa4dea80bd53dcbb3
aliaksei davydau <davidoff.aleksei@gmail.com>
2021-03-15 17:51:05 +0300

    feat: add simple chain task

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,26 +1,61 @@
+'use strict';
 const CustomError = require("../extensions/custom-error");
 
-const chainMaker = {
-  getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }
-};
+// const chainMaker = {
+//   getLength() {
+//     return chainMaker.chain.length;
+//   },
+//   addLink(value) {
+//     if (value !== undefined && typeof value !== 'string') {
+//       value = String(value);
+//     }
+//     chainMaker.chain.push(`( ${value} )`);
+//   },
+//   removeLink(position) {
+//     chainMaker.chain.splice(position - 1, 1);
+//   },
+//   reverseChain() {
+//     chainMaker.chain.reverse();
+//   },
+//   finishChain() {
+//     return chainMaker.chain.join(`~~`);
+//   },
+//   chain: []
+// };
+// chainMaker.addLink(function () {}).addLink('2nd').addLink('3rd').removeLink(2).reverseChain().finishChain();
 
+class ChainMaker {
+  chain = [];
+
+  addLink(value) {
+    this.chain.push(value);
+    return this;
+  }
+
+  finishChain() {
+    const chain = this.chain
+      .map(v => `( ${v} )`)
+      .join('~~');
+
+    this.chain = [];
+    return chain;
+  }
+
+  getLength() {
+    return this.chain.length;
+  }
+
+  removeLink(position) {
+    this.chain.splice(position - 1, 1);
+    return this;
+  }
+
+  reverseChain() {
+    this.chain.reverse();
+    return this;
+  }
+}
+const chainMaker = new ChainMaker();
 module.exports = chainMaker;
+
+
