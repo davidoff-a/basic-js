6a7db7cbf3bcfc41856a3df05b993867b9d4aa98
aliaksei davydau <davidoff.aleksei@gmail.com>
2021-03-24 09:57:07 +0300

    fix: fix some checks on carbon-dating task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,19 +3,22 @@
 
 const MODERN_ACTIVITY = 15;
 const HALF_LIFE_PERIOD = 5730;
-
+/* dateSample('0'); */
 module.exports = function dateSample(sampleActivity) {
+	let data=0;
 	if (
 		typeof sampleActivity !== 'string' ||
-		sampleActivity === NaN ||
-		Number.parseFloat(sampleActivity) < 0 ||
-		Number.parseFloat(sampleActivity) > MODERN_ACTIVITY
+		isNaN(parseFloat(sampleActivity)) ||
+		Number.parseFloat(sampleActivity) <= 0 ||
+		Number.parseFloat(sampleActivity) > MODERN_ACTIVITY ||
+		!isFinite(sampleActivity)
 	) {
 		return false;
 	} else {
-		return Math.ceil(
-			Math.log(MODERN_ACTIVITY / parseFloat(sampleActivity)) /
+		data = Math.ceil(
+				Math.log(MODERN_ACTIVITY / parseFloat(sampleActivity)) /
 				(Math.log(2) / HALF_LIFE_PERIOD)
 		);
 	}
+	return data;
 };
